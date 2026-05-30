094b15df2b740d21587121de5ba92274688a236a
Сизова Анасатсия <aesizova@edu.hse.ru>
2026-05-30 21:29:57 +0300

    Реализован класс Ingredient

---
+++ recipes.py
@@ -0,0 +1,27 @@
+class Ingredient:
+    def __init__(self, name: str, quantity: float, unit: str):
+        self.name = name
+        self.unit = unit
+        self.quantity = quantity
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        val = float(value)
+        if val <= 0:
+            raise ValueError("Количество должно быть положительным")
+        self._quantity = val
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}"
+
+    def __repr__(self):
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+
+    def __eq__(self, other):
+        if not isinstance(other, Ingredient):
+            return False
+        return self.name == other.name and self.unit == other.unit
