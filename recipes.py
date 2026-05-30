2c370cfa07eb6ce026b8ed2b3a2ebb7ac76505b6
Сизова Анасатсия <aesizova@edu.hse.ru>
2026-05-30 23:59:31 +0300

    Реализован класс Recipe

--- recipes.py
+++ recipes.py
@@ -25,3 +25,31 @@ class Ingredient:
         if not isinstance(other, Ingredient):
             return False
         return self.name == other.name and self.unit == other.unit
+    
+class Recipe:
+    def __init__(self, title: str, ingredients: list):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient):
+        for item in self.ingredients:
+            if item.name == ingredient.name and item.unit == ingredient.unit:
+                item.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio) -> bool:
+        return isinstance(ratio, (int, float)) and ratio > 0
+
+    def scale(self, ratio: float):
+        new_ingredients = [Ingredient(i.name, i.quantity * ratio, i.unit) for i in self.ingredients]
+        return Recipe(self.title, new_ingredients)
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        ingredients_str = ", ".join(str(i) for i in self.ingredients)
+        return f"Рецепт '{self.title}': {ingredients_str}"
+
